566b05c376a8195a106eb64724410ef92f19824b
cfloyd252 <cfloyd252@gmail.com>
2020-10-23 17:20:19 -0700

    Completed Day 4.

--- day_4.js
+++ day_4.js
@@ -44,28 +44,29 @@ function readLine() {
 
 function Person(initialAge){
   // Add some more code to run some checks on initialAge
-  let age = initialAge;
+let age = initialAge;
 
-  if (initialAge < 0) {
-    console.log('Age is not valid, setting age to 0.')
+if (initialAge < 0) {
+  console.log('Age is not valid, setting age to 0.')
 
-    age = 0;
-  }
-  this.amIOld=function(){
-    // Do some computations in here and print out the correct statement to the console
-    if (age < 13) {
-      console.log('You are young');
-    } else if (age >= 13 && age < 18) {
-      console.log('You are a teenager');
-    } else {
-      console.log('You are old')
-    }
-  };
-  this.yearPasses=function(){
-    // Increment the age of the person in here
-    age++;
-  };
+  age = 0;
 }
+
+this.amIOld=function(){
+  // Do some computations in here and print out the correct statement to the console
+  if (age < 13) {
+    console.log('You are young.');
+  } else if (age >= 13 && age < 18) {
+    console.log('You are a teenager.');
+  } else {
+    console.log('You are old.');
+  }
+};
+
+this.yearPasses=function(){
+  // Increment the age of the person in here
+  age++;
+};
 
 function main() {
 
